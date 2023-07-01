172db3a45e9baace416701cfa5d3919577f36fe0
Dana Batali <dana@cannerycoders.com>
2023-07-01 08:25:48 -0700

    MacOS - 1st step toward migration to staticly linked fluidsynth.

--- build/osxsign.js
+++ build/osxsign.js
@@ -1,40 +1,8 @@
-// here we sign the app, more notes in __Root.jsmk.
-// currently we're launched from osxFixup.sh
-
-// https://melatonin.dev/blog/how-to-code-sign-and-notarize-macos-audio-plugins-in-ci/
-
-const path = require("path");
-const buildname = path.parse(process.argv[1]).name; // ie build4electron
-process.env.DEBUG=`electron-osx-sign ${buildname}`;
-// process.env.DEBUG=`electron-packager electron-osx-sign electron-notarize ${buildname}`;
-
-const { execSync } = require('child_process');
-const pkg = require("./package.json"); // for version
-const d = require("debug")(buildname);
-
-const appDir = process.argv[2] || path.join(process.env.FIDDLEINST, "Fiddle.app");
-
-// https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
-async function DoitOSX()
-{
-    // let appDir =  "../_install/clang-arm64-darwin-vanilla-debug/Fiddle-0.4.0/Fiddle-darwin-arm64/Fiddle.app";
-    let appPath = path.join(__dirname, appDir);
-    const {signAsync} = require("electron-osx-sign");
-    d("code-signing");
-    await signAsync({
-        app: `${appDir}`,
-        platform: "darwin", //  not "mas" (not through the app Store)
-        type: "distribution",
-        entitlements: "../build/entitlements.plist",
-        "entitlements-inherit": "../build/entitlements.plist",
-        "gatekeeper-assess": false, 
-        hardenedRuntime: true,
-    });
-}
-
-async function Doit()
-{
-    await DoitOSX()
-}
-
-Doit()
+//
+// https://developer.apple.com/forums/thread/130855
+// just iterate over all the files in the package
+//
+// codesign --sign 807CCBABA311632D43C4E01B0624F4C76ED9925B --force --timestamp \
+//    --options runtime 
+//    --entitlements ../build/entitlements.plist 
+//    file
