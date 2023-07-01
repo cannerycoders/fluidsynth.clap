d7c55c29a5e3b224536170652bfa61af9c77b013
Dana Batali <dana@cannerycoders.com>
2023-07-01 15:19:20 -0700

    rough-in osx signing.

--- build/osxsign.js
+++ build/osxsign.js
@@ -2,7 +2,7 @@
 // https://developer.apple.com/forums/thread/130855
 // just iterate over all the files in the package
 //
-// codesign --sign 807CCBABA311632D43C4E01B0624F4C76ED9925B --force --timestamp \
+// codesign --sign magic --force --timestamp \
 //    --options runtime 
 //    --entitlements ../build/entitlements.plist 
 //    file
