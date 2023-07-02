7a360741c9139461daf78d78cc897cd51e5f3352
Dana Batali <dana@cannerycoders.com>
2023-07-02 10:19:49 -0700

    New install help, dmg construction.  Notarization is next.

--- build/darwin/osxpackage.js
+++ build/darwin/osxpackage.js
@@ -1,27 +1,40 @@
 //
 // hdiutil create -volname MyPlugin -srcfolder input.clap MyPlugin.dmg
 const path = require("path");
+const fs = require("fs");
 const appdmg = require("appdmg");
-if(process.argv.length != 5)
+if(process.argv.length != 6)
 {
-    console.error("usage: node osxpackage.js pluginName inputDir dmgPath");
+    console.error("usage: node osxpackage.js pluginName inputDir installHelp.rtf dmgPath");
     process.exit(-1);
 }
-let [_0, _1, pluginName, inputDir, dmgPath] = process.argv;
+let [_0, _1, pluginName, inputDir, installHelp, dmgPath] = process.argv;
 
 let cfg = {
-    basepath: process.cwd(),
     target: dmgPath,
+    basepath: ".",
     specification: {
         "title": `${pluginName} Installer`,
         "icon": path.join(inputDir, "Contents/Resources/clap.icns"),
-        "background": "build/darwin/background.png",
+        "background": "build/darwin/installbgd.png",
         "contents": [
-            { "x": 448, "y": 344, "type": "link", "path": "/Library/Audio/Plug-Ins/CLAP" },
-            { "x": 192, "y": 344, "type": "file", "path": inputDir }
+            // grid-locations are quantized
+            { "x": 415, "y": 375, "type": "link", "path": "/Library/Audio/Plug-Ins/CLAP" },
+            { "x": 20, "y": 375, "type": "file", "path": installHelp },
+            { "x": 185, "y": 375, "type": "file", "path": inputDir },
         ]
     },
 };
+
+try
+{
+    fs.unlinkSync(dmgPath);
+}
+catch(err)
+{
+    if(err.code != "ENOENT")
+        console.log(JSON.stringify(err, null, 2));
+}
 
 // console.log("cfg:\n" + JSON.stringify(cfg, null, 2));
 // process.exit(0);
