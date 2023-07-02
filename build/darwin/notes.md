53b3ea75c424eef9f1a42b6a27410900399c2c3a
Dana Batali <dana@cannerycoders.com>
2023-07-02 12:10:01 -0700

    OSXtsigning/packaging checkpoint.

--- build/darwin/notes.md
+++ build/darwin/notes.md
@@ -1,4 +1,16 @@
 ## build notes osx
 
+## dyld headeaches
+
+Conclusion is to statically link fluid (into our open-source plugin).
+Homebrew produces static libs for glib, etc, but not sndfile, etc.
+
+So we diable both readline and sndfile for our plugin.
+
+## Signing and Notarization
 
 https://melatonin.dev/blog/how-to-code-sign-and-notarize-macos-audio-plugins-in-ci
+
+spctl misleading:
+
+https://developer.apple.com/forums/thread/723090
--- build/darwin/osxnotarize.js
+++ build/darwin/osxnotarize.js
@@ -6,30 +6,40 @@
 let fs = require("fs"); // require("fs/promises");
 let path = require("path");
 let execFileSync = require("child_process").execFileSync;
-let devId = process.env.APPLEID_APPLICATION;
-if(!devId)
+let appleId = process.env.APPLEID;
+let teamId = process.env.APPLEID_TEAM;
+let accessToken = process.env.APPLEID_ACCESS_TOKEN;
+if(!appleId || !teamId || !accessToken)
 {
-    console.error("APPLEID_APPLICATION not found in environment.")
+    console.error("APPLEID, APPLEID_TEAM, APPLEID_ACCESS_TOKEN required in environment.")
     process.exit(-1);
 }
 if(process.argv.length != 3)
 {
-    console.error("usage: node osxsign.js path_to_pkg_root");
+    console.error("usage: node osxnotarize.js myplugin.dmg");
     process.exit(-1);
 }
-let projRoot = process.argv[2];
-let arglist = [
-    "--force", "-s", devId, "--deep", "--strict", "--timestamp",
-    projRoot,
+let notaryPkg = process.argv[2];
+let argv = [
+    "xcrun", "notarytool", "submit", notaryPkg, 
+      "--apple-id", appleId,
+      "--password", accessToken,
+      "--team-id", teamId, 
+      "--wait",
 ];
 
 try
 {
-    let stdout = execFileSync("codesign", arglist).toString().trim();
+    let stdout = execFileSync(argv[0], argv.slice(1)).toString().trim();
     if(stdout.length)
-        console.log(`INFO codesign:\n${stdout}`);
+        console.log(`INFO osxnotarize:\n${stdout}`);
+    
+    argv = ["xcrun", "stapler", "staple", notaryPkg]
+    stdout = execFileSync(argv[0], argv.slice(1)).toString().trim();
+    if(stdout.length)
+        console.log(`INFO osxnotarize/stapler:\n${stdout}`);
 }
 catch(err)
 {
-    console.error(`ERROR codesign:\n${err}`);
+    console.error(`ERROR osxnotarize:\n${err}`);
 }
