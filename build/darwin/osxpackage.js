9153149fdee465bfb42968d356966cdadb8d508b
Dana Batali <dana@cannerycoders.com>
2023-07-01 17:47:55 -0700

    osx create .dmg file.

--- build/darwin/osxpackage.js
+++ build/darwin/osxpackage.js
@@ -1,8 +1,7 @@
 //
 // hdiutil create -volname MyPlugin -srcfolder input.clap MyPlugin.dmg
-
-let execFileSync = require("child_process").execFileSync;
-let devId = process.env.APPLEID_APPLICATION;
+const path = require("path");
+const appdmg = require("appdmg");
 if(process.argv.length != 5)
 {
     console.error("usage: node osxpackage.js pluginName inputDir dmgPath");
@@ -10,17 +9,28 @@ if(process.argv.length != 5)
 }
 let [_0, _1, pluginName, inputDir, dmgPath] = process.argv;
 
-let arglist = [
-    "create", "-volname", pluginName, "-srcfolder", inputDir, dmgPath
-];
+let cfg = {
+    basepath: process.cwd(),
+    target: dmgPath,
+    specification: {
+        "title": `${pluginName} Installer`,
+        "icon": path.join(inputDir, "Contents/Resources/clap.icns"),
+        "background": "build/darwin/background.png",
+        "contents": [
+            { "x": 448, "y": 344, "type": "link", "path": "/Library/Audio/Plug-Ins/CLAP" },
+            { "x": 192, "y": 344, "type": "file", "path": inputDir }
+        ]
+    },
+};
 
-try
-{
-    let stdout = execFileSync("hdiutil", arglist).toString().trim();
-    if(stdout.length)
-        console.log(`INFO hdiutil:\n${stdout}`);
-}
-catch(err)
-{
-    console.error(`ERROR hdiutil:\n${err}`);
-}
+// console.log("cfg:\n" + JSON.stringify(cfg, null, 2));
+// process.exit(0);
+
+const ee = appdmg(cfg);
+ee.on("finish", function() {
+    console.log("done...");
+});
+
+ee.on("error", function(err) {
+    console.error("ERROR " + err);
+});
